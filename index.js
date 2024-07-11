0e1d703a5cd2f7937075f6322c660196876be737
alex-7-njoroge <njorogekinyanjui02@gmail.com>
2024-07-11 14:41:23 +0300

    code lab-2

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Alex Njoroge'
+
+const welcomeMessage = 'Welcome to Flatbook, Alex Njoroge!';
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, ALEX NJOROGE!";
+
+const shortGreeting =  "Welcome, A!"
